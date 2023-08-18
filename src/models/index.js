463e988d1c8e6294c113c6cfa11e4cbeceb292e9
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-17 22:50:10 -0300

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,17 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
 const User = require("./User");
 
+
 //Product -> categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
+
+//Cart -> userId
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+//Cart -> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
